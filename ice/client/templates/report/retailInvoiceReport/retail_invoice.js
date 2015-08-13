8a18fa804efc1b05fa454925c1b801451a541663
kevin <faithful.7up@gmail.com>
2015-08-13 16:40:14 +0700

    add auto print

--- ice/client/templates/report/retailInvoiceReport/retail_invoice.js
+++ ice/client/templates/report/retailInvoiceReport/retail_invoice.js
@@ -19,78 +19,16 @@
 //     }
 
 // });
-Template.ice_invoiceReportGen.onRendered(function () {
-    setTimeout(function () {
-        window.print();
-        window.close();
-    }, 2500);
-});
 /************ Generate *************/
 Template.ice_invoiceReportGen.helpers({
     data: function () {
         var self = this;
-        var data = {
-            title: {},
-            header: [],
-            content: [],
-            footer: [],
-        };
-
-        /********* Title *********/
-        var company = Cpanel.Collection.Company.findOne();
-        data.title = {
-            company: company.khName,
-            address: company.khAddress,
-            telephone: company.telephone
-        };
-
-        /********* Header ********/
-        var customerDoc = Ice.Collection.Customer.findOne(self.customerId);
-        var date = moment(self.date).format('YYYY-MM-DD');
-        var time = moment(self.date).format('HH:mm:ss');
-        var type = '';
-        var getOrder = Ice.Collection.Order.findOne(self.orderId);
-         
-                // var getGroup = Ice.Collection.OrderGroup.findOne({_id: self.groupId, iceCustomerId: self.customerId, startDate: {$lt: self.endDate}, endDate:{$gt: self.startDate}});
-        // dueDate = '' + getGroup.startDate + '-' + getGroup.endDate;
-        
-        if(customerDoc.customerType !== 'general'){
-            type = customerDoc.customerType + ' ថ្ងៃ';
+        var id = JSON.stringify(self);
+        var callRetailInvoice = Meteor.callAsync(id, 'reatailInvoice', self);
+        if(!callRetailInvoice.ready()){
+            return false;
         }
-        else{
-            type = 'general';
-        }
-        data.header = [
-            {col1: '#: ' + self.orderId, col2: 'បុគ្គលិក: ' + getOrder._staff.name },
-            {col1: 'អតិថិជន: ' + customerDoc.name, col2: 'ប្រភេទ: ' + type},
-            { col1: 'កាលបរិច្ឆេទ: ' + date, col2: 'ម៉ោង: ' + time }
-        ];
-
-        /********* Content & Footer *********/
-        var content = [];
-        var itemsDetail = getOrder.iceOrderDetail
-        itemsDetail.forEach(function (item) {
-            item.price = formatKH(item.price);
-            item.amount = formatDollar(item.amount);
-            item.discount = item.discount == undefined ? '' : item.discount + '%'
-            content.push(item);
-        });
-        content.push(itemsDetail);
-        if (content.length > 0) {
-            data.content = content;
-            data.footer = {
-                subtotal: formatKH(getOrder.subtotal),
-                discount: getOrder.discount == undefined ? '' : getOrder.discount,
-                total: formatKH(getOrder.total),
-                totalInDollar: formatDollar(getOrder.totalInDollar),
-                paidAmount: formatKH(getOrder.paidAmount),
-                outstandingAmount: formatKH(getOrder.outstandingAmount)
-            }
-            return data;
-        } else {
-            data.content.push({index: 'no results'});
-            return data;
-        }
+        return callRetailInvoice.result();
     },
 
     itemName: function(id){
