83847cd317d30f912534fef8d7a7ff39dddff4ed
kevin <faithful.7up@gmail.com>
2015-08-15 14:39:28 +0700

    done with payment popup

--- ice/client/templates/payment/payment_url/payment_url.js
+++ ice/client/templates/payment/payment_url/payment_url.js
@@ -1,34 +1,10 @@
 var datePicker, fillInDetail, selectCustomer, selectInvoice;
 PaymentUrl = new ReactiveObj(); 
 Template.ice_paymentUrlInsertTemplate.onRendered(function() {
-  selectCustomer(this.data);
+  datePicker(this.data._id);
 });
 
 Template.ice_paymentUrlInsertTemplate.events({
-  'change [name="customerId"]': function(e) {
-    var customer;
-    customer = $(e.currentTarget).val();
-    Ice.ListForReportState.set('customer', customer);
-  },
-  'keyup [name="orderId_orderGroupId"]': function(e) {
-    var currentInvoice, currentInvoiceId, type;
-    currentInvoiceId = $(e.currentTarget).val();
-    datePicker(currentInvoiceId);
-    type = Ice.ListForReportState.get('type');
-    if (type === 'general') {
-      currentInvoice = Ice.Collection.Order.findOne(currentInvoiceId);
-      PaymentUrl.set('oldPaidAmount', currentInvoice.paidAmount);
-      $('[name="dueAmount"]').val(currentInvoice.outstandingAmount);
-      $('[name="paidAmount"]').val(currentInvoice.outstandingAmount);
-      $('[name="outstandingAmount"]').val(0);
-    } else {
-      currentInvoice = Ice.Collection.OrderGroup.findOne(currentInvoiceId);
-      PaymentUrl.set('oldPaidAmount', currentInvoice.paidAmount);
-      $('[name="dueAmount"]').val(currentInvoice.outstandingAmount);
-      $('[name="paidAmount"]').val(currentInvoice.outstandingAmount);
-      $('[name="outstandingAmount"]').val(0);
-    }
-  },
   'keyup [name="paidAmount"]': function() {
     var dueAmount, paidAmount;
     dueAmount = parseInt($('[name="dueAmount"]').val());
@@ -37,7 +13,6 @@ Template.ice_paymentUrlInsertTemplate.events({
     }catch(e){
       console.log(e)
     }
-
     if (parseFloat(paidAmount) > dueAmount) {
       $('[name="paidAmount"]').val(dueAmount);
       $('[name="outstandingAmount"]').val(0);
@@ -50,50 +25,6 @@ Template.ice_paymentUrlInsertTemplate.events({
 });
 
 datePicker = function(currentInvoiceId) {
-  var maxDate, paymentDate, payments;
-  maxDate = '';
-  payments = Ice.Collection.Payment.find({
-    orderId_orderGroupId: currentInvoiceId
-  });
-  if (payments !== void 0) {
-    payments.forEach(function(payment) {
-      maxDate = maxDate > payment.paymentDate ? maxDate : payment.paymentDate;
-    });
-    maxDate;
-
-  }
   paymentDate = $('[name="paymentDate"]');
-  if (maxDate === '') {
-    return DateTimePicker.dateTime(paymentDate);
-  } else {
-    return paymentDate.data('DateTimePicker').minDate(maxDate);
-  }
-};
-
-selectCustomer = function(self) {
-  self = self;
-  debugger
-  return Meteor.setTimeout(function() {
-    $('[name="customerId"]').val(self.iceCustomerId);
-    $('label.customerName').text(self.iceCustomerId + ' | ' + self._customer.name);
-    Ice.ListForReportState.set('customer', self.iceCustomerId);
-    selectInvoice(self._id);
-    fillInDetail(self.outstandingAmount, self.paidAmount, self.outstandingAmount);
-    datePicker(self._id)
-  }, 100);
-};
-
-selectInvoice = function(invoiceId) {
-  return Meteor.setTimeout(function() {
-    $('[name="orderId_orderGroupId"]').val(invoiceId);
-    $('.orderId').text(invoiceId);
-  }, 100);
-
-};
-
-fillInDetail = function(dueAmount, paidAmount, outstandingAmount) {
-  $('[name="paidAmount"]').val(outstandingAmount);
-  $('[name="dueAmount"]').val(dueAmount);
-  $('[name="outstandingAmount"]').val(0);
-  return PaymentUrl.set('oldPaidAmount', parseInt(paidAmount));
+  return DateTimePicker.dateTime(paymentDate); 
 };
