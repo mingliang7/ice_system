beae29400669e73cd3f297bef8d978e812838141
kevin <faithful.7up@gmail.com>
2015-11-26 10:45:52 +0700

    finish adding lending and container

--- ice/server/collectionCache/lending.js
+++ ice/server/collectionCache/lending.js
@@ -3,3 +3,9 @@ Ice.Collection.Lending.cacheDoc('customer', Ice.Collection.Customer, ['name',
 ], {
   refField: 'customerId'
 });
+
+Ice.Collection.Lending.cacheDoc('staff', Ice.Collection.Staffs, ['name',
+  'telephone', 'address'
+], {
+  refField: 'staffId'
+});
